b75b2f5414da145177c48354c128df9a9224cf08
partavocado <89282211+part-avocado@users.noreply.github.com>
2024-11-04 12:34:55 -0500

    added some funny text
    because funny

--- helloWorld.groovy
+++ helloWorld.groovy
@@ -1,3 +1,3 @@
 import com.neuronrobotics.bowlerstudio.BowlerKernel
 // Have BowlerStudio speak messages
-BowlerKernel.speak("welcome to bowler studio")
+BowlerKernel.speak("No more bowler studio. He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He. He He He He He He He He He He He, He, He He He He He He He He He He, He He He He He He He He He He He He He He He He He He. He He He He He He He He He He He He He He He He He He He He He He He He He He He He He He, Oops")
